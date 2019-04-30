5e33a768d2a5922efc0f6a2ca15c34ed77e99357
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-30 19:08:20 +0200

    Improve main.c

--- main.c
+++ main.c
@@ -8,42 +8,43 @@ void START(void);
 void MUOVI_SPRITES_ESEMPIO(void);
 void WAIT_FOR_SORT(void);
 
-extern char SPRX[];
-extern char SPRY[];
-extern char SPRC[];
-extern char SPRF[];
-// extern char NUMSPRITES;
-extern char SPRUPDATEFLAG;
-extern char SPRIRQCOUNTER;
+extern unsigned char SPRX[];
+extern unsigned char SPRY[];
+extern unsigned char SPRC[];
+extern unsigned char SPRF[];
+extern unsigned char NUMSPRITES;
+extern unsigned char SPRUPDATEFLAG;
+extern unsigned char SPRIRQCOUNTER;
 extern unsigned short SPRITE_GFX;
 
 
 /******************/
 int main()
-{	
-	unsigned char XX = 0;
-	unsigned char i;
+{    
+    unsigned char XX = 0;
+    unsigned char i;
     START();
-    SPRF[30]=(SPRITE_GFX&0x3fff)>>6;
-    SPRF[31]=(SPRITE_GFX&0x3fff)>>6;
-	SPRX[30]=30;
-	SPRX[31]=130;
-	clrscr();
-	while(1) 
-	{
-		gotoxy(1,1); cprintf("%u",SPRUPDATEFLAG);
-	
-		SPRY[30]=XX;
-		SPRY[31]=255-XX;
-		for(i=0;i<30;++i)
-		{
+    for(i=0;i<32;++i)
+    {
+        SPRF[i] = (SPRITE_GFX&0x3fff)>>6;        
+    }
+    SPRX[30]=30;
+    SPRX[31]=130;
+    clrscr();
+    while(1) 
+    {
+        gotoxy(1,1); cprintf("%u",SPRUPDATEFLAG);
+    
+        SPRY[30]=XX;
+        SPRY[31]=255-XX;
+        for(i=0;i<30;++i)
+        {
             SPRC[i] = i;
-            SPRF[i] = (SPRITE_GFX&0x3fff)>>6;
-			SPRX[i] = XX+4*i;
-			SPRY[i] = 55+(i & 7)*24;
-		}
-		++XX;
+            SPRX[i] = XX+4*i;
+            SPRY[i] = 55+(i & 7)*24;
+        }
+        ++XX;
         SPRUPDATEFLAG = 1;
     }
-	return 0;
+    return 0;
 }
