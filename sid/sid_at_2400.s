4ff2117c6d36b02f825380967d8b773034e4b627
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-05-11 17:48:24 +0200

    Fix c128 wit no sid cfg

--- sid/sid_at_2400.s
+++ sid/sid_at_2400.s
@@ -1,3 +1,5 @@
 .segment "SID"
-.incbin "./Ghostbusters_Theme_at_2400.sid", $7e
+;.incbin "./Ghostbusters_Theme_at_2400.sid", $7e
+.incbin "./Acid_Mix_at_2400.sid", $7e
+;.incbin "./sid/I_Want_More_Than_3_Channels_at_2400.sid", $7e
 .code
